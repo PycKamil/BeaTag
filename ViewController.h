e36c90e85a53a0b766f6da4c3f664c84a330f607
Kamil Pyć <kamil@jouret-mobile.be>
2014-10-11 00:05:00 +0200

    initial commit

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeaTag
+//
+//  Created by Kamil Pyć on 10/10/14.
+//  Copyright (c) 2014 Kamil Pyć. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
