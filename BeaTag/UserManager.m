0bb827c86a0ee829481c9a9352fef2b51182e759
Anna Kwiecinska <kwia@viessmann.com>
2014-10-11 07:31:03 +0200

    user manager interface first version

---
+++ BeaTag/UserManager.m
@@ -0,0 +1,29 @@
+//
+//  UserManager.m
+//  BeaTag
+//
+//  Created by Anna Kwiecińska on 11/10/14.
+//  Copyright (c) 2014 Kamil Pyć. All rights reserved.
+//
+
+#import "UserManager.h"
+
+@implementation UserManager
+
+static UserManager *instance;
+
+
++ (UserManager *)sharedInstance
+{
+    static UserManager *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[UserManager alloc] init];
+        // init
+    });
+    return sharedInstance;
+}
+
+
+
+@end
