9f5380b47f81717854b69c5f23188964370c13c5
Kamil Pyć <kamil@jouret-mobile.be>
2014-10-11 06:10:58 +0200

    added screen with camera and gallery selection

---
+++ BeaTag/SelectActionViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelectActionViewController.h
+//  BeaTag
+//
+//  Created by Kamil Pyć on 10/11/14.
+//  Copyright (c) 2014 Kamil Pyć. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectActionViewController : UIViewController
+
+@end
